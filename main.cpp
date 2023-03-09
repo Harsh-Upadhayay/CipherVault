4b3178c1050661fcd62f4aa6067692fc164c7a08
Harsh Upadhayay <harshupadhayay906@gmail.com>
2023-03-09 23:00:46 +0530

    check for all implemented cipher

--- main.cpp
+++ main.cpp
@@ -3,19 +3,20 @@
 #include "CaesarDecrypter.h"
 #include "RailFenceEncrypter.h"
 #include "RailFenceDecrypter.h"
+#include "PlayFairDecrypter.h"
+#include "PlayFairEncrypter.h"
+
 
 using namespace std;
 
 int main()
 {
 
-    RailFenceCipherEncrypter pf;
-    RailFenceCipherDecrypter pfD(pf.getKey());
+    PlayFairEncrypter pf;
+    PlayFairDecrypter pfD(pf.getKey());
 
-    string msg = "defend the east wall";
+    string msg = "defend the east wall!";
     string cipher = pf.encrypt(msg);
-
-    cout << pf.getKey() << "\n";
 
     cout << cipher << "\n\n";
     cout << pfD.decrypt(cipher);
